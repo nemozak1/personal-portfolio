b7c83a28d2e86ab96146375367e5452e061ee53e
nemo <nemozak1@gmail.com>
2024-08-06 16:50:18 +0100

    init commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { ReactTyped } from "react-typed";
+
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <ReactTyped
+        startWhenVisible
+        className={styles.title}
+        strings={["Nemo Godebski-Pedersen"]}
+        typeSpeed={100}
+        cursorChar="_"
+      ></ReactTyped>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
